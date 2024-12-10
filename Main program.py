f5464599b07d6472f31042a692ac139c43ff7659
Cedar647 <temporarycedar@gmail.com>
2024-12-10 11:15:21 +0000

    Update Main program.py
    def draw_text()

--- Main program.py
+++ Main program.py
@@ -5,12 +5,17 @@ dimension_w = 1200 #screen width
 dimension_h = 800 #screen height
 
 screen = pygame.display.set_mode((dimension_w, dimension_h))
+pygame.display.set_caption("Game")
+
+def draw_text(text, font, color, x, y):
+    img = font.rander(text, True, color)
+    screen.blit(x,y)
 
 run = True
 while run:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-
+           run = False
+    pass
 pygame.quit()
